a5394b07b46bee37182f3133ebfedc310e545416
Paraj Shah <65371747+parajshah@users.noreply.github.com>
2020-09-24 03:14:06 +0530

    Update Calculator_GUI.py

--- Calculator_GUI.py
+++ Calculator_GUI.py
@@ -1,112 +1,95 @@
 from tkinter import *
+import parser
 import math
 
-expression = ""
 
-def press(num):
-    global expression
-    expression += str(num)
-    eqn.set(expression)
+root = Tk()
+root.title('Calculator')
+root.geometry('354x320')
+root.minsize(354,320)
+root.maxsize(354,320)
 
-def clear_expression():
-    global expression
-    expression = ""
-    eqn.set(expression)
-
-def square():
-    global expression
+pos = 0
+def get_variables(num):
+    global pos
+    display.insert(pos,num)
+    pos += 1
+ 
+def calculate():
+    entire_string = display.get()
     try:
-        total = str(eval(expression) ** 2)
-        expression = total
-        eqn.set(total)
-    except:
-        eqn.set("Cannot find square of empty or incomplete expression!")
-        expression  = ""
+        a = parser.expr(entire_string).compile()
+        result = eval(a)
+        clear_all()
+        display.insert(0,result)
+    except Exception:
+        clear_all()
+        display.insert(0,"Error")
+ 
+def get_operation(operator):
+    global pos
+    length = len(operator)
+    display.insert(pos,operator)
+    pos += length
+ 
+def clear_all():
+    display.delete(0,END)
+ 
+def undo():
+    entire_string = display.get()
+    if len(entire_string):
+        new_string = entire_string[:-1]
+        clear_all()
+        display.insert(0,new_string)
+    else:
+        clear_all()
+        display.insert(0,"Error")
 
-def square_root():
-    global expression
+def fact():
+    entire_string = display.get()
     try:
-        total = str(math.sqrt(eval(expression)))
-        expression = total
-        eqn.set(total)
-    except:
-        eqn.set("Cannot perform squareroot on empty or incomplete expression!")
-        expression  = ""
+        result = math.factorial(int(entire_string))
+        clear_all()
+        display.insert(0,result)
+    except Exception:
+        clear_all()
+        display.insert(0,"Error")
 
-def reciprocal():
-    global expression
-    try:
-        total = str(1 / eval(expression))
-        expression = total
-        eqn.set(total)
-    except:
-        eqn.set("Cannot perform reciprocal on empty or incomplete expression!")
-        expression  = ""
-    
+display = Entry(root, font=("Calibri",14),bg="white",fg="black",bd=0)
+display.grid(row=1,columnspan=6,sticky=N+E+W+S, ipady = 8)
+ 
+Button(root,text="1",command = lambda :get_variables(1), width = 7, height = 3).grid(row=2,column=0, sticky=N+S+E+W)
+Button(root,text="2",command = lambda :get_variables(2), width = 7, height = 3).grid(row=2,column=1, sticky=N+S+E+W)
+Button(root,text="3",command = lambda :get_variables(3), width = 7, height = 3).grid(row=2,column=2, sticky=N+S+E+W)
+ 
+Button(root,text="4",command = lambda :get_variables(4), width = 7, height = 3).grid(row=3,column=0, sticky=N+S+E+W)
+Button(root,text="5",command = lambda :get_variables(5), width = 7, height = 3).grid(row=3,column=1, sticky=N+S+E+W)
+Button(root,text="6",command = lambda :get_variables(6), width = 7, height = 3).grid(row=3,column=2, sticky=N+S+E+W)
 
-def press_equal():
-    global expression
-    try:
-        total = str(eval(expression))
-        eqn.set(total)
-        expression = total
-    except:
-        eqn.set("Error occured!")
-        expression = ""
+Button(root,text="7",command = lambda :get_variables(7), width = 7, height = 3).grid(row=4,column=0, sticky=N+S+E+W)
+Button(root,text="8",command = lambda :get_variables(8), width = 7, height = 3).grid(row=4,column=1, sticky=N+S+E+W)
+Button(root,text="9",command = lambda :get_variables(9), width = 7, height = 3).grid(row=4,column=2, sticky=N+S+E+W)
+ 
+Button(root,text="AC",command =lambda :clear_all(), width = 7, height = 3).grid(row=5,column=0, sticky=N+S+E+W)
+Button(root,text="0",command = lambda :get_variables(0), width = 7, height = 3).grid(row=5,column=1, sticky=N+S+E+W)
+Button(root,text=".",command =lambda :get_variables("."), width = 7, height = 3).grid(row=5, column=2, sticky=N+S+E+W)
+ 
+# Basic Operations
+Button(root,text="+",command = lambda :get_operation("+"), width = 7, height = 3).grid(row=2,column=3, sticky=N+S+E+W)
+Button(root,text="-",command = lambda :get_operation("-"), width = 7, height = 3).grid(row=3,column=3, sticky=N+S+E+W)
+Button(root,text="*",command = lambda :get_operation("*"), width = 7, height = 3).grid(row=4,column=3, sticky=N+S+E+W)
+Button(root,text="/",command = lambda :get_operation("/"), width = 7, height = 3).grid(row=5,column=3, sticky=N+S+E+W)
 
+Button(root,text="pi",command = lambda :get_operation("*3.14"), width = 7, height = 3).grid(row=2,column=4, sticky=N+S+E+W)
+Button(root,text="mod",command = lambda :get_operation("%"), width = 7, height = 3).grid(row=3,column=4, sticky=N+S+E+W)
+Button(root,text="(",command = lambda :get_operation("("), width = 7, height = 3).grid(row=4,column=4, sticky=N+S+E+W)
+Button(root,text="exp",command = lambda :get_operation("**"), width = 7, height = 3).grid(row=5,column=4, sticky=N+S+E+W)
 
-window = Tk()
-window.title("Calculator")
+Button(root,text=u"\u232B",command = lambda :undo(), width = 7, height = 3).grid(row=2,column=5, sticky=N+S+E+W)
+Button(root,text="x!", command = lambda: fact(), width = 7, height = 3).grid(row=3,column=5, sticky=N+S+E+W)
+Button(root,text=")",command = lambda :get_operation(")"), width = 7, height = 3).grid(row=4,column=5, sticky=N+S+E+W)
+Button(root,text="^2",command = lambda :get_operation("**2"), width = 7, height = 3).grid(row=5,column=5, sticky=N+S+E+W)
 
-eqn = StringVar()
-
-expression_field = Label(window, textvariable = eqn, width = 50)
-expression_field.grid(columnspan = 4, ipadx = 20, ipady = 5)
-
-# Numbers
-
-button9 = Button(window, text = "9", command = lambda: press(9), height = 3, width = 12)
-button9.grid(row = 2, column = 2)
-button8 = Button(window, text = "8", command = lambda: press(8), height = 3, width = 12)
-button8.grid(row = 2, column = 1)
-button7 = Button(window, text = "7", command = lambda: press(7), height = 3, width = 12)
-button7.grid(row = 2, column = 0)
-button6 = Button(window, text = "6", command = lambda: press(6), height = 3, width = 12)
-button6.grid(row = 3, column = 2)
-button5 = Button(window, text = "5", command = lambda: press(5), height = 3, width = 12)
-button5.grid(row = 3, column = 1)
-button4 = Button(window, text = "4", command = lambda: press(4), height = 3, width = 12)
-button4.grid(row = 3, column = 0)
-button3 = Button(window, text = "3", command = lambda: press(3), height = 3, width = 12)
-button3.grid(row = 4, column = 2)
-button2 = Button(window, text = "2", command = lambda: press(2), height = 3, width = 12)
-button2.grid(row = 4, column = 1)
-button1 = Button(window, text = "1", command = lambda: press(1), height = 3, width = 12)
-button1.grid(row = 4, column = 0)
-button0 = Button(window, text = "0", command = lambda: press(0), height = 3, width = 12)
-button0.grid(row = 5, column = 0)
-
-# Operations
-
-one_by_x = Button(window, text = "1/x", command = reciprocal, height = 3, width = 12)
-one_by_x.grid(row = 1, column = 0)
-square = Button(window, text = "x**2", command = square, height = 3, width = 12)
-square.grid(row = 1, column = 1)
-square_root = Button(window, text = "sqrt(x)", command = square_root, height = 3, width = 12)
-square_root.grid(row = 1, column = 2)
-plus = Button(window, text = "+", command = lambda: press("+"), height = 3, width = 12)
-plus.grid(row = 1, column = 3)
-minus = Button(window, text = "-", command = lambda: press("-"), height = 3, width = 12)
-minus.grid(row = 2, column = 3)
-multiply = Button(window, text = "x", command = lambda: press("*"), height = 3, width = 12)
-multiply.grid(row = 3, column = 3)
-divide = Button(window, text = "/", command = lambda: press("/"), height = 3, width = 12)
-divide.grid(row = 4, column = 3)
-equal = Button(window, text = "=", command = press_equal, height = 3, width = 12)
-equal.grid(row = 5, column = 3)
-decimal = Button(window, text = ".", command = lambda: press("."), height = 3, width = 12)
-decimal.grid(row = 5, column = 1)
-clear = Button(window, text = "Clear", command = clear_expression, height = 3, width = 12)
-clear.grid(row = 5, column = 2)
-
-window.mainloop()
+Button(root,text="=",command = lambda :calculate(), width = 7, height = 3).grid(columnspan=6, sticky=N+S+E+W)
+ 
+root.mainloop()
